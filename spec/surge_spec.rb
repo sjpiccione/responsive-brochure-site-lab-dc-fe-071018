73c3dd1deb22c7d9ae0023a13ab1e77a26c2c8dc
Steven Piccione <sjpiccione@gmail.com>
2018-08-03 00:31:20 +0000

    Done.

--- spec/surge_spec.rb
+++ spec/surge_spec.rb
@@ -1,15 +1,15 @@
 require_relative './spec_helper'
 
-SOURCE_FILE="my_url.txt"
+SOURCE_FILE="http://dysfunctional-observation.surge.sh/"
 
-describe "The file #{SOURCE_FILE}" do
+describe "The file #{http://dysfunctional-observation.surge.sh/}" do
   it 'should be present' do
-    file_is_present = File.exists?(SOURCE_FILE)
-    expect(file_is_present).to be_truthy, "Make sure you created a file called #{SOURCE_FILE} with your URL in it."
+    file_is_present = File.exists?(http://dysfunctional-observation.surge.sh/)
+    expect(file_is_present).to be_truthy, "Make sure you created a file called #{http://dysfunctional-observation.surge.sh/} with your URL in it."
   end
 
   it 'should contain a url to a surge.sh URL like http://dusty-rose.surge.sh' do
-    url = File.open(SOURCE_FILE).readline
+    url = File.open(http://dysfunctional-observation.surge.sh/).readline
     expect(url.match(/^http.*\.surge\.sh\z/)).to be_truthy
   end
 end
